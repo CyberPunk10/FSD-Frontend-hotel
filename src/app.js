d20e2ead1093515f72d1562c3f65302109df2f0e
CyberPunk10 <konstantin21_arx@gmail.com>
2020-07-01 19:46:03 +0300

    Started development pages 'cards' and developmment buttons

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import './main.sass'
 import './components/dropdown/dropdown.js'
 import './components/like-btn/like-btn.js'
 import './components/rate-btn/rate-btn.js'
+import './components/range-slider/range-slider.js'
 
 // проверка ES6 синтаксиса
 let i = 'проверка ES6 синтаксиса'
