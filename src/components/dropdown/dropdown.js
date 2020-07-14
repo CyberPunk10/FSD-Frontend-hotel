8ce327b6a07b191bb6256628d79e6ae45b2588a1
CyberPunk_xiaomi <exemple@xiaomi.ru>
2020-07-14 15:35:10 +0300

    Correct dropdown and other

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -4,46 +4,59 @@ let dropdowns = document.querySelectorAll(".dropdown__content")
 
 // Навешиваем на все dropdowns функцию по клику
 dropdown__btns.forEach(element => {
-  element.addEventListener('click', openDropdown);
+  element.addEventListener('click', toggleDropdown);
 });
 
 
 /* Когда пользователь нажимает на кнопку,
 переключение между скрытием и отображением раскрывающегося содержимого */
-function openDropdown() {
-  event.target.nextElementSibling.classList.toggle("show")
+function toggleDropdown() {
+  event.target.nextElementSibling.classList.toggle("dropdown__content_show")
   event.target.classList.toggle("dropdown__btn_expanded")
 }
 
 
-// Закройте выпадающее меню, если пользователь щелкает за его пределами
 window.onclick = function (event) {
-  if (!event.target.matches('.dropdown__btn')) {
+
+  // Закрываем выпадающее меню, если пользователь щелкает за его пределами
+  if (!event.target.matches('.dropdown__btn')) {    // Метод matches позволяет проверить, удовлетворяет ли элемент указанному CSS селектору
+
     for (let i = 0; i < dropdown__btns.length; i++) {
-      if (dropdown__btns[i].classList.contains('dropdown__btn_expanded')) {
-        dropdown__btns[i].classList.remove('dropdown__btn_expanded');
-      }
-      if (dropdowns[i].classList.contains('show')) {
-        dropdowns[i].classList.remove('show');
+
+      // если атрибут 'show-always' НЕ содeржит 'true', то dropdown сворачиваем. иначе оставляем несвернутым
+      if (!dropdown__btns[i].dataset.showAlways) {
+
+        if (dropdown__btns[i].classList.contains('dropdown__btn_expanded')) {
+          dropdown__btns[i].classList.remove('dropdown__btn_expanded');
+        }
+
+        if (dropdowns[i].classList.contains('dropdown__content_show')) {
+          dropdowns[i].classList.remove('dropdown__content_show');
+        }
+
       }
     }
-  } else {
+
+  } else {    // Иначе обрабатываем клик по dropdown
 
     let drop_id = event.target.nextElementSibling.id  // id dropdown, по которому произошёл клик
 
     for (let i = 0; i < dropdown__btns.length; i++) {
-      
-      if (dropdowns[i].id != drop_id) {
-  
-        if (dropdown__btns[i].classList.contains('dropdown__btn_expanded')) {
-          dropdown__btns[i].classList.remove('dropdown__btn_expanded');
-        }
-        if (dropdowns[i].classList.contains('show')) {
-          dropdowns[i].classList.remove('show');
-        }
-  
-      }
 
+      if (dropdowns[i].id != drop_id) {
+
+        // если атрибут 'show-always' НЕ содeржит 'true', то dropdown сворачиваем. иначе оставляем несвернутым
+        if (!dropdown__btns[i].dataset.showAlways) {
+
+          if (dropdown__btns[i].classList.contains('dropdown__btn_expanded')) {
+            dropdown__btns[i].classList.remove('dropdown__btn_expanded');
+          }
+          if (dropdowns[i].classList.contains('dropdown__content_show')) {
+            dropdowns[i].classList.remove('dropdown__content_show');
+          }
+
+        }
+      }
     }
   }
 }
