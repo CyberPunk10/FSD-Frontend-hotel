c7f53ad9c995766dd4bccd777425b8284f53ebe5
CyberPunk_xiaomi <exemple@xiaomi.ru>
2020-08-06 16:55:47 +0300

    Development select Minin practics

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 import './main.sass'
 import './components/form-element/form-element.js'
 import './components/dropdown/dropdown.js'
-import {handlerDropdown} from './components/dropdown/dropdown.js'
-import {closeOthersDropdowns} from './components/dropdown/dropdown.js'
+import { handlerDropdown } from './components/dropdown/dropdown.js'
+import { closeOthersDropdowns } from './components/dropdown/dropdown.js'
+import { Select } from './components/select/select.js'
 import './components/buttons/like-btn/like-btn.js'
 import './components/buttons/rate-btn/rate-btn.js'
 import './components/range-slider/range-slider.js'
@@ -12,6 +13,11 @@ let i = 'проверка ES6 синтаксиса'
 let k = value => console.log(value)
 k(i)
 
+const select = new Select('#select', {
+
+})
+
+window.select = select
 
 document.addEventListener('click', function (e) {
 
@@ -19,7 +25,7 @@ document.addEventListener('click', function (e) {
 
   // if click on dropdown
   if (target.closest('.dropdown')) {
-    handlerDropdown (target.closest('.dropdown'))
+    handlerDropdown(target.closest('.dropdown'))
   } else {
     closeOthersDropdowns(target)
   }
